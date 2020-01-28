b5c6070411987874721aad4546bacf2eb0d04c94
Leonardo <leo.lima21051990@gmail.com>
2020-01-28 15:23:21 -0300

    complete ball detection

--- src/mission.py
+++ src/mission.py
@@ -22,34 +22,43 @@ class camera:
     self.start = time.time()   
 
   def callback(self, data):
-    # timer count and font
+    # setup timer and font
     timer = int(time.time() - self.start)
     font = cv2.FONT_HERSHEY_SIMPLEX
 
     # convert img to cv2
     cv2_frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
 
-    # color range
+    ### COLOR DETECTION ###
+    # define range of yellow color
     yellowLower = (20, 100, 100)
     yellowUpper = (32, 255, 255)
 
-    # hsv convert and mask create
+    # hsv color-space convert
     hsv = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2HSV)
+
+    # Threshold the HSV image to get only blue colors
     maskYellow = cv2.inRange(hsv, yellowLower, yellowUpper)
+
+    # erosion and dilation for noise removal
     maskYellow = cv2.erode(maskYellow, None, iterations=2)
     maskYellow = cv2.dilate(maskYellow, None, iterations=2) 
+
+    # find contours of image (cv2.CHAIN_APPROX_SIMPLE is for memory saves)
     cnt_yellow = cv2.findContours(maskYellow.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     
-    # circle detection
+    ### CIRCLE DETECTION ###    
     contours_poly = []
     centers = []
     radius = [] 
     
+    # approximate contours to polygons + get bounding rects and circles
     for index, obj_cnt in enumerate(cnt_yellow):
       contours_poly.append(cv2.approxPolyDP(obj_cnt, 0.009 * cv2.arcLength(obj_cnt, True), True))
       aux1, aux2 = cv2.minEnclosingCircle(contours_poly[index])
       centers.append(aux1)
       radius.append(aux2)
+      # if the camera find the sphere
       if(len(contours_poly[index]) > 10):
         # draw a circle in sphere and put a warning message
         cv2.circle(cv2_frame, (int(centers[index][0]), int(centers[index][1])), int(radius[index]), (0, 0, 255), 5) 
@@ -59,7 +68,7 @@ class camera:
     cv2.putText(cv2_frame, str(timer) + 's', (20, 60), font, 2, (50, 255, 50), 5) 
     cv2.putText(cv2_frame, str(time.ctime()), (10, 700), font, 2, (50, 255, 50), 6)
 
-    # convert img to ros and pub image
+    # convert img to ros and pub image in a topic
     ros_frame = self.bridge.cv2_to_imgmsg(cv2_frame, "bgr8")
     self.pub.publish(ros_frame)
 

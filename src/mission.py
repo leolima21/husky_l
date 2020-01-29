c5b3dc43dac798ebacc176fa91125d2967c92904
Leonardo <leo.lima21051990@gmail.com>
2020-01-29 14:23:28 -0300

    control

--- src/mission.py
+++ src/mission.py
@@ -6,20 +6,27 @@ import cv2
 import time
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
+from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 from std_msgs.msg import Int32
 from sensor_msgs.msg import Image
 
-class camera:
+class Camera:
   def __init__(self):
     # create a node
     rospy.init_node('node_camera_mission', anonymous=True)
-    # publisher object
+    # image publisher object
     self.pub = rospy.Publisher('camera/mission', Image, queue_size=10)
+    # cmd_vel publisher object
+    self.velocity_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     # bridge object
     self.bridge = CvBridge()
     # timer var
     self.start = time.time()   
+    # radius control
+    self.linear_vel_control = Controller(5, -5, 0.01, 0, 0)
+    # x position control
+    self.angular_vel_control = Controller(5, -5, 0.01, 0, 0)
 
   def callback(self, data):
     # setup timer and font
@@ -63,9 +70,17 @@ class camera:
         # draw a circle in sphere and put a warning message
         cv2.circle(cv2_frame, (int(centers[index][0]), int(centers[index][1])), int(radius[index]), (0, 0, 255), 5) 
         cv2.putText(cv2_frame, 'BOMB HAS BEEN DETECTED!', (20, 130), font, 2, (0, 0, 255), 5)
-        print(radius) 
-        print(centers)
-
+        # controller actions
+        linear_vel = self.linear_vel_control.calculate(1, 174, radius[0])
+        angular_vel = self.angular_vel_control.calculate(1, 640, centers[0][0])
+        self.cmd_vel_pub(linear_vel, angular_vel) 
+        # print info
+        print('CONTROL INFO :')
+        print('radius: ' + str(radius[0]))
+        print('linear vel: ' + str(linear_vel))        
+        print('x position: ' + str(centers[0][0]))
+        print('angular vel: ' + str(angular_vel))
+        print('##################################')
     # merge timer info to frame
     cv2.putText(cv2_frame, str(timer) + 's', (20, 60), font, 2, (50, 255, 50), 5) 
     cv2.putText(cv2_frame, str(time.ctime()), (10, 700), font, 2, (50, 255, 50), 6)
@@ -74,19 +89,51 @@ class camera:
     ros_frame = self.bridge.cv2_to_imgmsg(cv2_frame, "bgr8")
     self.pub.publish(ros_frame)
 
-  #def pid_calculate(self):
-
-
   def listener(self):
     # subscribe to a topic
     rospy.Subscriber('camera/image_raw', Image, self.callback)  
     # simply keeps python from exiting until this node is stopped
     rospy.spin()
+  
+  def cmd_vel_pub(self, linear, angular):
+    vel_msg = Twist()
+    vel_msg.linear.x = linear
+    vel_msg.angular.z = angular
+    self.velocity_publisher.publish(vel_msg)
+
+class Controller:
+  sat_max = 0
+  sat_min = 0
+  kp = 0
+  ki = 0
+  kd = 0
+  error_integral = 0 
+  error_prev = 0 
+
+  def __init__ (self, sat_max, sat_min, kp, ki, kd):
+    self.sat_max = sat_max 
+    self.sat_min = sat_min 
+    self.kp = kp 
+    self.ki = ki 
+    self.kd = kd 
+    
+  def calculate(self, time, setpoint, process):
+    self.error = setpoint - process
+    self.error_integral =+ self.error
+    control_input = self.kp*self.error + self.ki*(self.error_integral)*time + self.kd*(self.error - self.error_prev)/time
+    
+    if (control_input > self.sat_max):
+      control_input = self.sat_max
+    elif (control_input < self.sat_min):
+      control_input = self.sat_min
+   
+    self.error_prev = self.error
+    return control_input  
 
 # main function
 if __name__	== '__main__':
   try:
-    cam_print = camera()  
+    cam_print = Camera()  
     cam_print.listener()  
   except rospy.ROSInterruptException:
     pass			
